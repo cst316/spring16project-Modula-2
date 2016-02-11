22c9b76f85e470aa4cf80abfb3fd096c0221ea5a
therealdavidhenderson <therealdavidhenderson@gmail.com>
2016-02-10 18:28:25 -0700

    Calendar view generates CalendarNodes
    Calendar view now generates CalendarNodes. Pushed mainly for Nelson to
    have something visual to add to.

--- src/net/sf/memoranda/ui/CalendarPanel.java
+++ src/net/sf/memoranda/ui/CalendarPanel.java
@@ -1,15 +1,21 @@
 package net.sf.memoranda.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CalendarPanel extends JPanel {
 	WorkPanel parentPanel = null;
 	JPanel mainPanel = new JPanel(new BorderLayout());
-	CalendarTaskbarPanel taskPanel = new CalendarTaskbarPanel();
-	JPanel periodPanel = new JPanel(new GridBagLayout());
+	CalendarTaskbarPanel taskPanel = new CalendarTaskbarPanel(this);
+	JPanel gridPanel = new JPanel(new GridBagLayout());
+	
+	GridBagConstraints gbc;
 	
     public CalendarPanel(WorkPanel _parentPanel) {
         try {
@@ -22,8 +28,24 @@ public class CalendarPanel extends JPanel {
     }
     
     void jbInit() throws Exception {
+    	for(int weekday = 0; weekday < 7; weekday++) {
+    		for(int row = 0; row < 5; row++) {
+    			JPanel gridCell = new JPanel();
+    			
+    			CalendarNode calendarNode = new CalendarNode();
+    			gridCell.add(calendarNode);
+    			gridCell.setBackground(Color.BLACK);
+    			
+    	        gbc = new GridBagConstraints();
+    	        gbc.gridx = weekday; gbc.gridy = row;
+    	        gbc.insets = new Insets(0, 0, 0, 0);
+    	        gbc.anchor = GridBagConstraints.CENTER;
+    	        gridPanel.add(gridCell, gbc);
+    		}
+    	}
+    	
     	mainPanel.add(taskPanel,BorderLayout.NORTH);
-    	mainPanel.add(periodPanel,BorderLayout.SOUTH);
+    	mainPanel.add(gridPanel,BorderLayout.SOUTH);
     	this.add(mainPanel);
     }
 }
