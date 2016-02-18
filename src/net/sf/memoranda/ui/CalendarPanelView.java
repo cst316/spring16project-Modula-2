effc3c6acaba9693de1354ef48d1882654bb6a65
therealdavidhenderson <therealdavidhenderson@gmail.com>
2016-02-18 14:31:41 -0700

    Fixed panel not redrawing on changing views
    CalendarView month/week/day view change removed the old children but did
    not repaint so the old panel would be visible.
    
    Also removed a debug print statement.

--- src/net/sf/memoranda/ui/CalendarPanelView.java
+++ src/net/sf/memoranda/ui/CalendarPanelView.java
@@ -339,7 +339,6 @@ public class CalendarPanelView extends JPanel {
 	        if(_parent.taskPanel.isShowEvents()) {
 	            Collection<Event> events = (Collection<Event>) EventsManager.getEventsForDate(date);
 	            for (Event event : events) {
-	            	System.out.println(start.get(Calendar.HOUR_OF_DAY) + ":" + start.get(Calendar.MINUTE) + " -> " + event.getTimeString() + " -> " + end.get(Calendar.HOUR_OF_DAY) + ":" + end.get(Calendar.MINUTE) + " = " + inTimespan(event, start, end));
 	            	if(inTimespan(event, start, end))
 	            		panelCell.getCalendarNode().addEvent(event);
 	            }
