2187c20958aebc6f8a40129476889d7b2b990e9e
agamezli <agamezli@asu.edu>
2016-02-27 20:36:57 -0700

    Worked on a basic taskbar for the psp fucntionality, its missing a project selector. still pretty much a prototype but i thought it would be usefull since luis and i are both working on psp. also created a class that does nothing yet for a Time recording, named TimeEntry.java

---
+++ src/net/sf/memoranda/TimeEntry.java
@@ -0,0 +1,5 @@
+package net.sf.memoranda;
+
+public class TimeEntry {
+
+}
---
+++ src/net/sf/memoranda/ui/PSPPanel.java
@@ -0,0 +1,24 @@
+package net.sf.memoranda.ui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.GregorianCalendar;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+
+public class PSPPanel extends JPanel {
+	
+ 
+ 
+    
+}
