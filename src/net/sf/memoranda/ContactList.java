2610a3ac4bf2242411fc62cbd216819819b1191d
Nelson Tran <nttran9@asu.edu>
2016-03-31 17:09:20 -0700

    Add core classes for storing contacts (Task #118)

---
+++ src/net/sf/memoranda/ContactList.java
@@ -0,0 +1,23 @@
+package net.sf.memoranda;
+
+import java.util.List;
+
+import nu.xom.Document;
+
+public interface ContactList {
+	
+	Contact addContact(String name,
+			           String email,
+			           String phone);
+	
+	void removeTimeEntry(Contact contact);
+	
+	List<Contact> getLog();
+	
+	int size();
+	
+	Project getProject();
+	
+	Document getXMLContent();
+	
+}
