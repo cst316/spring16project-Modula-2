369b89a8fac9a75d7a3aec1604b01d24eca57b4b
therealdavidhenderson <therealdavidhenderson@gmail.com>
2016-04-14 22:39:34 -0700

    TaskPlanningLog Dialog Beautification

--- src/net/sf/memoranda/ui/TaskPlanningRecordLogDialog.java
+++ src/net/sf/memoranda/ui/TaskPlanningRecordLogDialog.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowEvent;
 import java.text.DateFormat;
@@ -90,83 +91,91 @@ public class TaskPlanningRecordLogDialog extends JDialog {
         // Top tier stuff (task # / task name)
         // Task # label
         taskNumLabel.setText("Task #:");
-        gbc.gridx = 0; gbc.gridy = 0;
+        gbc.gridx = 0; gbc.gridy = 0; gbc.insets = new Insets(8,16,0,8);
         taskLayout.add(taskNumLabel, gbc);
         
         // Task # spinner
-        gbc.gridx = 1; gbc.gridy = 0;
+        gbc.gridx = 1; gbc.gridy = 0; gbc.insets = new Insets(8,8,0,8);
         taskLayout.add(taskNum,gbc);
         
         // Task name label
         taskNameLabel.setText("Task name:");
-        gbc.gridx = 2; gbc.gridy = 0;
+        gbc.gridx = 2; gbc.gridy = 0; gbc.insets = new Insets(8,16,0,8);
         taskLayout.add(taskNameLabel, gbc);
         
         // Task name text area
         taskName.setLineWrap(true);
         taskName.setColumns(64);
-        gbc.gridx = 3; gbc.gridy = 0;
+        gbc.gridx = 3; gbc.gridy = 0; gbc.insets = new Insets(8,8,0,16);
         taskLayout.add(taskName,gbc);
         
         // Middle tier stuff
         // Planned hour label
         plannedHourLabel.setText("Planned hours:");
-        gbc.gridx = 0; gbc.gridy = 0;
+        gbc.gridx = 0; gbc.gridy = 0; gbc.insets = new Insets(16,8,0,16);
         logLayout.add(plannedHourLabel,gbc);
         
         // Planned hour spinner
-        gbc.gridx = 1; gbc.gridy = 0;
+        gbc.gridx = 1; gbc.gridy = 0; gbc.gridwidth = 2; gbc.insets = new Insets(16,8,0,16);
         logLayout.add(plannedHour,gbc);
         
         // Planned value label
         plannedValueLabel.setText("Planned value:");
-        gbc.gridx = 2; gbc.gridy = 0;
+        gbc.gridx = 3; gbc.gridy = 0; gbc.gridwidth = 1; gbc.insets = new Insets(16,8,0,16);
         logLayout.add(plannedValueLabel,gbc);
         
         // Planned value
-        gbc.gridx = 3; gbc.gridy = 0;
+        gbc.gridx = 4; gbc.gridy = 0; gbc.insets = new Insets(16,8,0,16);
         logLayout.add(plannedValue,gbc);
         
         // Planned date label
         plannedDateLabel.setText("Planned date:");
-        gbc.gridx = 4; gbc.gridy = 0;
+        gbc.gridx = 5; gbc.gridy = 0; gbc.insets = new Insets(16,8,0,16);
         logLayout.add(plannedDateLabel,gbc);
         
         // Planned date
         SimpleDateFormat sdf = new SimpleDateFormat();
         sdf = (SimpleDateFormat) DateFormat.getDateInstance(DateFormat.SHORT);
         plannedDate.setEditor(new JSpinner.DateEditor(plannedDate, sdf.toPattern()));
-        gbc.gridx = 5; gbc.gridy = 0;
+        gbc.gridx = 6; gbc.gridy = 0; gbc.insets = new Insets(16,8,0,16);
         logLayout.add(plannedDate,gbc);
         
         // Completed label
         completedCheckboxLabel.setText("Completed");
-        gbc.gridx = 0; gbc.gridy = 1;
+        gbc.gridx = 1; gbc.gridy = 1; gbc.insets = new Insets(32,32,0,4);
         logLayout.add(completedCheckboxLabel,gbc);
         
         // Completed checkbox
-        gbc.gridx = 1; gbc.gridy = 1;
+        completedCheckbox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	completedCheckbox_actionPerformed(e);
+            }
+        });
+        
+        gbc.gridx = 2; gbc.gridy = 1; gbc.insets = new Insets(32,4,0,8);
         logLayout.add(completedCheckbox,gbc);
         
         // Earned value label
         earnedValueLabel.setText("Planned value:");
-        gbc.gridx = 2; gbc.gridy = 1;
+        gbc.gridx = 3; gbc.gridy = 1; gbc.insets = new Insets(32,8,0,16);
         logLayout.add(earnedValueLabel,gbc);
         
         // Earned value
-        gbc.gridx = 3; gbc.gridy = 1;
+        gbc.gridx = 4; gbc.gridy = 1; gbc.insets = new Insets(32,8,0,16);
+        earnedValue.setEnabled(completedCheckbox.isSelected());
         logLayout.add(earnedValue,gbc);
         
         // Actual date label
         actualDateLabel.setText("Actual date:");
-        gbc.gridx = 4; gbc.gridy = 1;
+        gbc.gridx = 5; gbc.gridy = 1; gbc.insets = new Insets(32,8,0,16);
         logLayout.add(actualDateLabel,gbc);
         
         // Actual date
         sdf = new SimpleDateFormat();
         sdf = (SimpleDateFormat) DateFormat.getDateInstance(DateFormat.SHORT);
         actualDate.setEditor(new JSpinner.DateEditor(actualDate, sdf.toPattern()));
-        gbc.gridx = 5; gbc.gridy = 1;
+        actualDate.setEnabled(completedCheckbox.isSelected());
+        gbc.gridx = 6; gbc.gridy = 1; gbc.insets = new Insets(32,8,0,16);
         logLayout.add(actualDate,gbc);
         
         // Bottom tier stuff (ok/cancel button)
@@ -180,7 +189,7 @@ public class TaskPlanningRecordLogDialog extends JDialog {
                 okB_actionPerformed(e);
             }
         });
-        gbc.gridx = 0; gbc.gridy = 0;
+        gbc.gridx = 0; gbc.gridy = 0; gbc.insets = new Insets(16,0,0,8);
         buttonLayout.add(okButton,gbc);
         
         // Cancel button
@@ -193,9 +202,11 @@ public class TaskPlanningRecordLogDialog extends JDialog {
                 cancelB_actionPerformed(e);
             }
         });
-        gbc.gridx = 1; gbc.gridy = 0;
+        gbc.gridx = 1; gbc.gridy = 0; gbc.insets = new Insets(16,0,0,0);
         buttonLayout.add(cancelButton,gbc);
-                
+        
+        // Clear insets
+        gbc.insets = new Insets(0,0,0,0);
         
         // Add top tier layout to mainLayout
         gbc.gridx = 0; gbc.gridy = 0;
@@ -206,14 +217,19 @@ public class TaskPlanningRecordLogDialog extends JDialog {
         mainLayout.add(logLayout, gbc);
         
         // Add bottom tier layout to mainLayout
-        gbc.gridx = 0; gbc.gridy = 2;
+        gbc.gridx = 0; gbc.gridy = 2; gbc.anchor = gbc.EAST;
         mainLayout.add(buttonLayout, gbc);
         
         // Add main layout to dialog
         this.add(mainLayout);
     }
    
-    void okB_actionPerformed(ActionEvent e) {
+    protected void completedCheckbox_actionPerformed(ActionEvent e) {
+		earnedValue.setEnabled(completedCheckbox.isSelected());
+		actualDate.setEnabled(completedCheckbox.isSelected());
+	}
+
+	void okB_actionPerformed(ActionEvent e) {
         this.dispose();
     }
 
