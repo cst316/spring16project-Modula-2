b51292776e12c5a89ce73dd627f5357b7409df75
esreeves <esreeves@asu.edu>
2016-04-16 20:11:45 -0700

    Forgot to add this file.

--- src/net/sf/memoranda/tests/NewProjectTest.java
+++ src/net/sf/memoranda/tests/NewProjectTest.java
@@ -1,14 +1,32 @@
 package net.sf.memoranda.tests;
 
+import java.util.Date;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import net.sf.memoranda.Project;
+import net.sf.memoranda.ProjectManager;
+import net.sf.memoranda.date.CalendarDate;
+import net.sf.memoranda.ui.ProjectDialog;
+import net.sf.memoranda.util.Local;
 
 public class NewProjectTest {
 
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		ProjectDialog dlg = new ProjectDialog(null, Local.getString("New project"));
+		CalendarDate startDate = new CalendarDate((Date) dlg.startDate.getModel().getValue());
+        CalendarDate endDate = null;
+        
+		CalendarDate startDateExpected = new CalendarDate((Date) dlg.startDate.getModel().getValue());
+        CalendarDate endDateExpected = null;
+        
+		Project prjInjected = ProjectManager.createProject("Test", "Something About a project", startDate, endDate);
+		Project prjExpected = ProjectManager.createProject("Test", "Something About a project", startDateExpected, endDateExpected);
+		
 		
 	}
 
@@ -18,6 +36,7 @@ public class NewProjectTest {
 	
 	@Test
 	public void testNewProject(){
+		
 		
 	}
 }
