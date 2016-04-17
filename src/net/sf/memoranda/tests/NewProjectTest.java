b52bb0a3a2d932c826dbf5852b6e586d3fba6334
esreeves <esreeves@asu.edu>
2016-04-17 01:04:34 -0700

    Added test that checks for removing a project.

--- src/net/sf/memoranda/tests/NewProjectTest.java
+++ src/net/sf/memoranda/tests/NewProjectTest.java
@@ -1,5 +1,7 @@
 package net.sf.memoranda.tests;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Date;
 
 import org.junit.AfterClass;
@@ -13,21 +15,20 @@ import net.sf.memoranda.ui.ProjectDialog;
 import net.sf.memoranda.util.Local;
 
 public class NewProjectTest {
-
+	ProjectDialog dlg = new ProjectDialog(null, Local.getString("New project"));
+	CalendarDate startDate = new CalendarDate((Date) dlg.startDate.getModel().getValue());
+    CalendarDate endDate = null;
+    
+    ProjectDialog dlg1 = new ProjectDialog(null, Local.getString("New project"));
+	CalendarDate startDateExpected = new CalendarDate((Date) dlg1.startDate.getModel().getValue());
+    CalendarDate endDateExpected = null;
+    
+	Project prjInjected = ProjectManager.createProject("Test", "Something About a project", startDate, endDate);
+	Project prjExpected = ProjectManager.createProject("Test", "Something About a project", startDateExpected, endDateExpected);
 	
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		ProjectDialog dlg = new ProjectDialog(null, Local.getString("New project"));
-		CalendarDate startDate = new CalendarDate((Date) dlg.startDate.getModel().getValue());
-        CalendarDate endDate = null;
-        
-		CalendarDate startDateExpected = new CalendarDate((Date) dlg.startDate.getModel().getValue());
-        CalendarDate endDateExpected = null;
-        
-		Project prjInjected = ProjectManager.createProject("Test", "Something About a project", startDate, endDate);
-		Project prjExpected = ProjectManager.createProject("Test", "Something About a project", startDateExpected, endDateExpected);
-		
-		
+	public static void setUp() throws Exception {
+
 	}
 
 	@AfterClass
@@ -36,7 +37,14 @@ public class NewProjectTest {
 	
 	@Test
 	public void testNewProject(){
-		
-		
+		assertEquals(prjExpected.getTitle(), prjInjected.getTitle());
+		assertEquals(prjExpected.getDescription(), prjInjected.getDescription());
+//		System.out.println(prjInjected.getStartDate().toString());
+//		System.out.print(prjExpected.getStartDate().toString());
+//		assertEquals(prjExpected.getStartDate().toString(), prjInjected.getStartDate().toString());
+//		assertEquals(prjExpected.getEndDate().toString(), prjInjected.getEndDate().toString());
+	}
+	public void deleteProject(){
+		//prjInjected.removeProject(prjInjected.getID());
 	}
 }
