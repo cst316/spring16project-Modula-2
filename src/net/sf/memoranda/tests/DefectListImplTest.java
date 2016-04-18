915e72f7484e84696b7698b0510f422ec14c8468
lequinta <lequinta@asu.edu>
2016-04-17 22:35:58 -0700

    Added editDefectList() to the unit tests

--- src/net/sf/memoranda/tests/DefectListImplTest.java
+++ src/net/sf/memoranda/tests/DefectListImplTest.java
@@ -135,6 +135,7 @@ public class DefectListImplTest {
 	
 	@Test
 	public void initializeBadDefectList() {
+		
 		assert(defectlist.getAllDefects().size() == 0);
 		assert(defectlist.getLastDefectId().equals("1"));
 		
@@ -270,6 +271,159 @@ public class DefectListImplTest {
 	@Test
 	public void editDefectList() {
 		
+		assert(defectlist.getAllDefects().size() == 0);
+		assert(defectlist.getLastDefectId().equals("1"));
+		
+		/*	Defect 1
+		 *	ID: 1
+		 *	Description: the first defect
+		 *	Date Found: 4/14/16
+		 *	Type: 30 Build, Package
+		 *	Est Fix Time: 34
+		 *	Injection: Planning
+		 *	Status: Active
+		 */
+		Phase injphase1 = null;
+		String inj1 = "Planning";
+		
+		for (Phase p : Phase.values()) {
+			if (inj1.equals(p.toString())) {
+				injphase1 = p;
+			}
+		}
+		
+		Phase remphase1 = null;
+		
+		Defect d1 = defectlist.createDefect(new CalendarDate("4/14/16"), "30 Build, Package", injphase1, Util.getMillisFromMinutes("34"), 
+				Util.getMillisFromMinutes("0"), null, remphase1, null, "the first defect", false);
+		
+		assert(defectlist.getAllDefects().size() == 1);
+		assert(defectlist.getLastDefectId().equals("2"));
+		
+		d1.editApproximateFixTimeInMillis(Util.getMillisFromMinutes("33"));
+		d1.editDescription("this is THE description");
+		
+		inj1 = "Test";
+		for (Phase p : Phase.values()) {
+			if (inj1.equals(p.toString())) {
+				injphase1 = p;
+			}
+		}
+		
+		d1.editInjection(injphase1);
+		
+		assert(d1.getApproximateFixTimeInMillis() == Util.getMillisFromMinutes("33"));
+		assert(d1.getDescription().equals("this is THE description"));
+		assert(d1.getInjection() == injphase1);
+		
+		
+		/*	Defect 2
+		 *	ID: 2
+		 *	Description: the secUND defect
+		 *	Date Found: 4/20/16
+		 *	Type: 70 Data
+		 *	Est Fix Time: 2
+		 *	Injection: Test
+		 *	Status: Active
+		 */
+		Phase injphase2 = null;
+		String inj2 = "Test";
+		
+		for (Phase p : Phase.values()) {
+			if (inj2.equals(p.toString())) {
+				injphase2 = p;
+			}
+		}
+		
+		Phase remphase2 = null;
+		
+		Defect d2 = defectlist.createDefect(new CalendarDate("4/20/16"), "70 Data", injphase2, Util.getMillisFromMinutes("2"), 
+				Util.getMillisFromMinutes("0"), null, remphase2, null, "the secUND defect", false);
+		
+		assert(defectlist.getAllDefects().size() == 2);
+		assert(defectlist.getLastDefectId().equals("3"));
+		
+		d2.editApproximateFixTimeInMillis(Util.getMillisFromMinutes("2222"));
+		d2.editDescription("this is THE second description");
+		
+		inj2 = "Planning";
+		for (Phase p : Phase.values()) {
+			if (inj2.equals(p.toString())) {
+				injphase2 = p;
+			}
+		}
+		
+		d2.editInjection(injphase2);
+		
+		assert(d2.getApproximateFixTimeInMillis() == Util.getMillisFromMinutes("2222"));
+		assert(d2.getDescription().equals("this is THE second description"));
+		assert(d2.getInjection() == injphase2);
+		
+		
+		/*	Defect 3
+		 *	ID: 3
+		 *	Description: the 3rd defect
+		 *	Date Found: 9/23/14
+		 *	Type: 50 Interface
+		 *	Est Fix Time: 1000
+		 *	Injection: Code Review
+		 *	Status: Completed
+		 *	Date Fixed: 4/14/16
+		 *	Act Time: 89
+		 *	Remove: Code Review
+		 *	Fix Reference: lazy
+		 */
+		Phase injphase3 = null;
+		String inj3 = "Code Review";
+		
+		for (Phase p : Phase.values()) {
+			if (inj3.equals(p.toString())) {
+				injphase3 = p;
+			}
+		}
+		
+		Phase remphase3 = null;
+		String rem3 = "Code Review";
+		
+		for (Phase p : Phase.values()) {
+			if (rem3.equals(p.toString())) {
+				remphase3 = p;
+			}
+		}
+		
+		Defect d3 = defectlist.createDefect(new CalendarDate("9/23/14"), "50 Interface", injphase3, Util.getMillisFromMinutes("1000"), 
+				Util.getMillisFromMinutes("89"), new CalendarDate("4/14/16"), remphase3, "lazy", "the 3rd defect", true);
+		
+		assert(defectlist.getAllDefects().size() == 3);
+		assert(defectlist.getLastDefectId().equals("4"));
+		
+		d1.editApproximateFixTimeInMillis(Util.getMillisFromMinutes("1"));
+		d1.editFixTimeInMillis(Util.getMillisFromMinutes("69"));
+		d1.editDescription("this is THE third description");
+		
+		inj3 = "Postmortem";
+		for (Phase p : Phase.values()) {
+			if (inj3.equals(p.toString())) {
+				injphase3 = p;
+			}
+		}
+		
+		d3.editInjection(injphase3);
+		
+		rem3 = "Test";
+		for (Phase p : Phase.values()) {
+			if (rem3.equals(p.toString())) {
+				remphase3 = p;
+			}
+		}
+		
+		d3.editInjection(remphase3);
+		
+		assert(d3.getApproximateFixTimeInMillis() == Util.getMillisFromMinutes("1"));
+		assert(d3.getFixTimeInMillis() == Util.getMillisFromMinutes("69"));
+		assert(d3.getDescription().equals("this is THE third description"));
+		assert(d3.getInjection() == injphase3);
+		assert(d3.getRemove() == remphase3);
 	}
 	
 	@Test
