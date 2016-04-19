b3317448b125d5f85129783a0e2be849960bc6e9
Nelson Tran <nttran9@asu.edu>
2016-04-19 13:58:16 -0700

    Started work on Summary Panel

---
+++ src/net/sf/memoranda/ui/SummaryPanel.java
@@ -0,0 +1,112 @@
+package net.sf.memoranda.ui;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+import net.sf.memoranda.CurrentProject;
+import net.sf.memoranda.Defect;
+import net.sf.memoranda.Phase;
+import net.sf.memoranda.TaskPlanningEntry;
+import net.sf.memoranda.TimeEntry;
+import net.sf.memoranda.TimeLogImpl;
+
+public class SummaryPanel extends JPanel {
+
+	PSPPanel parentPanel = null;
+	
+	private HashMap<Integer, Integer> plannedValue;
+	private HashMap<Integer, Integer> actualValue;
+	private HashMap<Phase, Integer> timeInPhase;
+	private HashMap<Phase, Integer> defectsInjected;
+	private HashMap<Phase, Integer> defectsRemoved;
+	
+	public SummaryPanel(PSPPanel parent) {
+		parentPanel = parent;
+		
+		plannedValue = new HashMap<>();
+		actualValue = new HashMap<>();
+		timeInPhase = new HashMap<>();
+		defectsInjected = new HashMap<>();
+		defectsRemoved = new HashMap<>();
+		
+        try {
+        	aggregatePSPData();
+            jbInit();
+        }
+        catch (Exception ex) {
+            new ExceptionDialog(ex);
+        }
+        
+        // Refresh summary panel whenever PSP data gets modified
+        TimeLogImpl.addTimeLogListener(() -> {
+			jbInit();
+		});
+        
+        PSPDefectPanel.addDefectListListener(() -> {
+        	jbInit();
+        });
+        
+        TimeLogImpl.addTimeLogListener(() -> {
+        	jbInit();
+        });
+	}
+	
+	void jbInit() {
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void aggregatePSPData() {
+		
+		// initialize maps
+		for (Phase phase : Phase.values()) {
+			timeInPhase.put(phase, 0);
+			defectsInjected.put(phase, 0);
+			defectsRemoved.put(phase, 0);
+		}
+		
+		// Planning Log
+		List<TaskPlanningEntry> tasks = CurrentProject.getTaskPlanningLog().getLog();
+		for (TaskPlanningEntry task : tasks) {
+			int plannedWeek = task.getPlannedDate().getWeek();
+			int actualWeek = task.getActualDate().getWeek();
+			
+			if (plannedValue.containsKey(plannedWeek))
+				plannedValue.put(plannedWeek, plannedValue.get(plannedWeek) + task.getPV());
+			else
+				plannedValue.put(plannedWeek, task.getPV());
+			
+			if (task.isComplete()) {
+				if (actualValue.containsKey(actualWeek))
+					actualValue.put(actualWeek, actualValue.get(actualWeek) + task.getEV());
+				else
+					actualValue.put(actualWeek, task.getPV());
+			}
+		}
+		
+		// Time Log
+		List<TimeEntry> entries = CurrentProject.getTimeLog().getLog();
+		for (TimeEntry entry : entries) {
+			Phase phase = entry.getPhase();
+			
+			if (phase != null)
+				timeInPhase.put(phase, timeInPhase.get(phase) + entry.getDeltaTime());
+		}
+		
+		// Defect Log
+		Collection<Defect> defects = CurrentProject.getDefectList().getAllDefects();
+		for (Defect defect : defects) {
+			Phase injectionPhase = Phase.valueOf(defect.getInjection());
+			Phase removalPhase = Phase.valueOf(defect.getRemove());
+			
+			if (injectionPhase != null)
+				defectsInjected.put(injectionPhase, defectsInjected.get(injectionPhase) + 1);
+			if (removalPhase != null)
+				defectsRemoved.put(removalPhase, defectsInjected.get(removalPhase) + 1);
+		}
+	}
+	
+}
