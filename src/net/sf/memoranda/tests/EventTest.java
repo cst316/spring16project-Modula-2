4707a8e7bf0d608dbf808efca4c5b1e4e780c9d7
lequinta <lequinta@asu.edu>
2016-04-19 21:21:44 -0700

    Fixed EventTest.java to correctly build in Travis-CI

--- src/net/sf/memoranda/tests/EventTest.java
+++ src/net/sf/memoranda/tests/EventTest.java
@@ -4,53 +4,53 @@ import static org.junit.Assert.*;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
 import net.sf.memoranda.date.CalendarDate;
 
 public class EventTest {
-//event dates
+	
+	//event dates
 	private CalendarDate sd1;
 	private CalendarDate sd2;
 	private CalendarDate sd3;
-
 	
-//descriptions
+	//descriptions
 	private String text1;
 	private String text2;	
 	private String text3;
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
 	@Before
 	public void setUp() throws Exception {
 		
-//start date declarations
+		//start date declarations
 		sd1 = new CalendarDate(10,2,2016);
 		sd2 = new CalendarDate(11,2,2016);
 		sd3 = new CalendarDate(10,2,2016);
 		
-		
-//text declarations
+		//text declarations
 		text1 = "This is text 1!";
 		text2 = "This text 2!";
 		text3 = "This text 3!";
 	}
+	
+	@Test
 	public void testEventCreation() {
 		//todo
 	}
-	public void testEventRemoval(){
+	
+	@Test
+	public void testEventRemoval() {
 		//todo
 	}
-	public void testEventEdit(){
+	
+	@Test
+	public void testEventEdit() {
 		//todo
 	}
-	public void testIsRepeatable(){
+	
+	@Test
+	public void testIsRepeatable() {
 		//todo
 	}
-
-
 }
