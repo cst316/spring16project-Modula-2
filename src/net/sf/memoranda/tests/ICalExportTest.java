3a9877d8b890fc4a1927542a52a0c08068f43026
therealdavidhenderson <therealdavidhenderson@gmail.com>
2016-04-21 21:08:49 -0700

    iCal Export event count Test

--- src/net/sf/memoranda/tests/ICalExportTest.java
+++ src/net/sf/memoranda/tests/ICalExportTest.java
@@ -2,15 +2,35 @@ package net.sf.memoranda.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Vector;
+
+import net.sf.memoranda.Event;
+import net.sf.memoranda.EventsManager;
+import net.sf.memoranda.util.ICalExporter;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ICalExportTest {
-
+	int recurringCount;
+	int nonrecurringCount;
+	int exceptionDateCount;
+	
+	net.fortuna.ical4j.model.Calendar iCal;
+	
 	@Before
 	public void setUp() throws Exception {
+		Vector<Event> recurringEvents = EventsManager.getRepeatableEvents();
+		recurringCount = recurringEvents.size();
+		nonrecurringCount = EventsManager.getNonrecurringEvents().size();
+		iCal = ICalExporter.getICalCalendar();
 		
+		exceptionDateCount = 0;
+		for(int i = 0; i < recurringEvents.size(); i++) {
+			if(recurringEvents.get(i).getExceptionDates().size() > 0)
+				exceptionDateCount++;
+		}
 	}
 
 	@After
@@ -18,7 +38,7 @@ public class ICalExportTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void iCalEventCountTest() {
+		assertTrue(iCal.getComponents().size() == recurringCount+nonrecurringCount+exceptionDateCount);
 	}
 }
