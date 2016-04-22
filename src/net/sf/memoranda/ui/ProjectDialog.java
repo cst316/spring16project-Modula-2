9f6a579c749ae96a983ad4a92571707e7d4c2652
Eric Reeves <esreeves@asu.edu>
2016-04-21 22:37:42 -0700

    Static analysis for some gui elements.

--- src/net/sf/memoranda/ui/ProjectDialog.java
+++ src/net/sf/memoranda/ui/ProjectDialog.java
@@ -427,8 +427,6 @@ public class ProjectDialog extends JDialog {
     
     void sdButton_actionPerformed(ActionEvent e) {
         //startCalFrame.setLocation(sdButton.getLocation());
-        this.getContentPane().getHeight();
-        this.getLayeredPane().add(startCalFrame);
         startCalFrame.setTitle(Local.getString("Start date"));
         startCalFrame.show();
     }
@@ -457,8 +455,7 @@ public class ProjectDialog extends JDialog {
         String description = dlg.descriptionTextArea.getText();
         CalendarDate startD = new CalendarDate((Date) dlg.startDate.getModel().getValue());
         CalendarDate endD = null;
-        if (dlg.endDateChB.isSelected())
-            endD = new CalendarDate((Date) dlg.endDate.getModel().getValue());
+        
         
         //Project prj = ProjectManager.createProject(title, description, startD, endD);
         /*if (dlg.freezeChB.isSelected())
